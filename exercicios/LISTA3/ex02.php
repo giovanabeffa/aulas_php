a8962c0ce8c32604c80677a05f91e9e3bcd9e076
Giovana Beffa <fernandagiovanabeffa@gmail.com>
2026-09-12 20:44:06 -0300

    Update ex02.php

--- exercicios/LISTA3/ex02.php
+++ exercicios/LISTA3/ex02.php
@@ -1,2 +1,12 @@
 <?php
 
+// Atribuição dos valores para cada chave
+$cliente = [
+    "nome" => "João",
+    "email" => "joao@email.com",
+    "telefone" => "(01) 2345-6789",
+    "cidade" => "Ourinhos"
+];
+
+// Acessando o dado através de sua chave nominal
+echo "O cliente {$cliente["nome"]} mora em {$cliente["cidade"]}. Contato: {$cliente["email"]}";
