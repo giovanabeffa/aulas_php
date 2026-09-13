d44c4ef1368ae386f7b35053974f97f1bd940d4e
Giovana Beffa <fernandagiovanabeffa@gmail.com>
2026-09-12 21:22:29 -0300

    Create ex04.php

---
+++ exercicios/LISTA3/ex04.php
@@ -0,0 +1,13 @@
+<?php
+
+//Criação do array multidimensional
+$produtos = [
+    ["id" => 1, "nome" => "Processador", "preco" => 1700.00],
+    ["id" => 2, "nome" => "Fonte", "preco" => 600.00],
+    ["id" => 3, "nome" => "Gabinete", "preco" => 250.00]
+];
+
+//Utilização do foreach para percorrer a matriz
+foreach ($produtos as $item) {
+    echo "<strong>Produto:</strong> {$item["nome"]} - <strong>Valor:</strong> R$ {$item["preco"]}<br>";
+};
