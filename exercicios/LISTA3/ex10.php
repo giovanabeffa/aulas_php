ccc43a666c7f81de9997b9b8cd06a69677b70d58
Giovana Beffa <fernandagiovanabeffa@gmail.com>
2026-09-12 22:36:21 -0300

    Update ex10.php

--- exercicios/LISTA3/ex10.php
+++ exercicios/LISTA3/ex10.php
@@ -2,7 +2,7 @@
 
 $faturamentoTotal = 0;
 
-$trasacoes = [
+$transacoes = [
     ["id" => 1, "valor" => 1200.00, "status" => "aprovado"],
     ["id" => 2, "valor" => 70.00, "status" => "estornado"],
     ["id" => 3, "valor" => 100.00, "status" => "fraude"],
@@ -10,6 +10,17 @@ $trasacoes = [
     ["id" => 2, "valor" => 5.00, "status" => "estornado"],
 ];
 
+foreach ($transacoes as $transacao) {
+    if ($transacao["status"] == "estornado") {
+        continue;
+} else if ($transacao["status"] == "fraude") {
+    echo "<span style:'color: red;'>Alestra de Segurança! Caixa bloqueado.</span>";
+    break;
+} else {
+    $faturamentoTotal += $transacao["aprovado"];
+}};
+
+echo "Faturamento total apurado: R$ $faturamentoTotal"
 
 
 ?>
