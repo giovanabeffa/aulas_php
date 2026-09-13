d73943b3a02eb66d2039b59096e67876971a82ad
Giovana Beffa <fernandagiovanabeffa@gmail.com>
2026-09-12 21:30:19 -0300

    Delete ex05..php

--- exercicios/LISTA3/ex05..php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-$ingressosDisponiveis = 10;
-
-//Criação do laço while que rode enquanto os ingressos disponíveis forem maiores que 0
-while ($ingressosDisponiveis > 0) {
-    echo "Ingresso validado. Restam: $ingressosDisponiveis.<br>";
-    $ingressosDisponiveis--;
-};
