5e2c0da39571ff78207286e507cc129ebd234783
Giovana Beffa <fernandagiovanabeffa@gmail.com>
2026-09-19 09:48:56 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+    // 1. Importa a base do HTML e o Header
+    require_once 'includes/cabecalho.php';
+?>
+
+<!-- 2. Conteúdo EXCLUSIVO e central desta tela -->
+ <div class="conteudo-principal">
+    <h1>Painel Administrativo</h1>
+    <p>Bem-vindo ao sistema de gestão modular.</p>
+ </div>
+
+ <?php
+//  3. Importa os fechamentos e o Footer
+require_once 'includes/rodape.php';
+?>
