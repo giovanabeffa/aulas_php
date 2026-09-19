02d3c9d20eedc2be044962aa9a5506e20684bf1c
Giovana Beffa <fernandagiovanabeffa@gmail.com>
2026-09-19 09:48:47 -0300

    Create cabecalho.php

---
+++ includes/cabecalho.php
@@ -0,0 +1,17 @@
+<!-- As tags principais abrem aqui, mas NÃO FECHAM neste arquivo -->
+ <!DOCTYPE html>
+ <html lang="pt-BR">
+ <head>
+    <meta charset="UTF-8">
+    <title>Sistema modular</title>
+ </head>
+ <body>
+    <header>
+        <h2>Logotipo Empresa</h2>
+        <nav>
+            <a href="index.php">Início</a> | 
+            <a href="alunos.php">Alunos</a>
+        </nav>
+    </header>
+    <main>
+    <!-- Repare que deixamos as tags main e body ABERTAS de propósito -->
