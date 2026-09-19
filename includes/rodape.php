e17b93c01f29204f9cfe67c92a0b62d33b6b7f5a
Giovana Beffa <fernandagiovanabeffa@gmail.com>
2026-09-19 09:48:53 -0300

    Create rodape.php

---
+++ includes/rodape.php
@@ -0,0 +1,10 @@
+<!-- Aqui fechamos todas as tags vitais que abrimos no cabeçalho -->
+
+    </main>
+
+    <footer>
+        <hr>
+        <p>Projeto Integtador II - &copy; 2026 UNIFIO</p>
+    </footer>
+    </body>
+    </html>
